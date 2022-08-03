a20554b49b6d41f009a62734081de0ba3464831e
claudioas <claudioasepulvedav@gmail.com>
2022-08-03 13:39:58 -0400

    Reasignación y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,30 @@
+var firstName; // se auto asigna Undefined
+firstName = 'Claudio';
+console.log(firstName);
+
+var lastName = 'Leonardo'; // declarar y asignar al mismo tiempo
+lastName = 'José'; // reasignar
+console.log(lastName);
+
+var secondName = 'David'; //declarar y asignando al mismo tiempo
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// let
+let fruit = 'Manzana'; // declarar y asignar
+fruit = 'kiwi'; // reasignar
+console.log(fruit);
+let fruit = 'Plátano';
+console.log();
+
+// const
+const animal = 'dog'; // declara y asigna
+// animal = 'cat'; // reasignando
+const animal = 'Snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("Suzuki");
+console.log(vehicles);
+vehicles.pop();
+console.log(vehicles);
