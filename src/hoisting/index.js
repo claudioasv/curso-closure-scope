4c65ff4098300f5e9a47a957674b98eed8419f03
claudioas <claudioasepulvedav@gmail.com>
2022-08-03 17:00:04 -0400

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+// var nameOfDog; // undefined
+
+// console.log(nameOfDog);
+// var nameOfDog = 'Luna';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
